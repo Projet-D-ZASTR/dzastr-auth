fcf770319a7b5c9fb4d5e920b0a4b493c2499f02
FabienArthurYnov <fabien.arthur@ynov.com>
2026-03-27 11:45:01 +0100

    fixed database connection to local

--- config/database.js
+++ config/database.js
@@ -4,12 +4,6 @@ import { env } from './env.js';
 export const sequelize = new Sequelize(env.db.url, {
   dialect: 'postgres',
   logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
 });
 
 export const initDatabase = async () => {
