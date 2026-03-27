0182e30abc2c0bc7b4ea11a406731a109ec87d3b
FabienArthurYnov <fabien.arthur@ynov.com>
2026-03-27 13:14:33 +0100

    added global servicetoken to swagger

--- swagger.js
+++ swagger.js
@@ -2,11 +2,15 @@ import swaggerJsdoc from 'swagger-jsdoc';
 
 const options = {
   definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'My API',
-      version: '1.0.0',
+    "openapi": "3.0.0",
+    "info": { "title": "My API", "version": "1.0.0" },
+    "components": {
+      "securitySchemes": {
+        "ServiceToken": { "type": "apiKey", "in": "header", "name": "x-service-token" },
+        "BearerAuth": { "type": "http", "scheme": "bearer", "bearerFormat": "JWT" }
+      }
     },
+    "security": [{ "ServiceToken": [] }],
   },
   apis: ['./swagger/*.swagger'], // where your routes are
 };
