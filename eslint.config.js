09d507827a9583618d46063b5ac21cd59ad5a748
Inarinokaze <camille.daverio@gmail.com>
2026-04-13 15:41:11 +0200

    #45 adding prettier to auth service

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   {
@@ -14,4 +15,5 @@ export default defineConfig([
       "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     },
   },
+  prettier,
 ]);
